ce56bd3b0c0da6bdf5224b628dd8284f55490a35
Fabio Silva <fabiovariant@hotmail.com>
2018-04-18 17:59:57 -0300

    Putting all in Dockerfile

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 8077,
+    port: process.env.PORT || 8077,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '',
